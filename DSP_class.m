9d8b4b5918d43969c2e18f0c6c69283fe5b272a4
salomon-castano <scastanoc@eafit.edu.co>
2022-04-13 18:05:09 +0200

    General improvements
    Refinement of the filter resolution (rise time) . Correction in the sintonizer and sincronizer setup. Removal of the guard. Implementation of a signal conditioning function. Correction of the index for the data start

--- DSP_class.m
+++ DSP_class.m
@@ -5,12 +5,14 @@ classdef DSP_class
        fc = 900e6;       % carrier frequency in Hz
        rb = 60e3;        % symbols (bits) per second
        ss = 5;           % samples per symbol
-       fs = 5e3;         % samples per frame
        QAM = 4;          % size of QAM constellation
-       guard = 100;      % guard length
+       plen = 26;        % preamble length
        delay = 500;      % approximate delay of the transmitter
+       fs                % samples per frame
        rs                % samples per second
        ls                %symbols per letter
+       mlen               % message length
+       
        
        % dependable variables and System Objects
        preamble
@@ -26,47 +28,52 @@ classdef DSP_class
        
    end
    methods
-      function a = setup(a) 
+      function a = setup(a, message) 
             % dependable var and System Objects initicalization
-            
+                        
             a.rs = a.ss*a.rb;   % samples per second
             
             a.ls = int32(8/log2(a.QAM)); %symbols per letter
+            
+            a.mlen = length(message)*a.ls; % message length in QAM symbols
           
-            a.preamble = comm.BarkerCode('SamplesPerFrame',13,...
+            a.preamble = comm.BarkerCode('SamplesPerFrame', a.plen,...
                'Length',13); 
            
            if a.QAM > 2
-               a.preamble = a.preamble()*complex(1,1);
+               a.preamble = complex(a.preamble(),fliplr(a.preamble()));
            else
                a.preamble = a.preamble();
            end
            
+           % samples per frame
+           a.fs = 2*(length(a.preamble) + a.mlen - 1)*a.ss; 
+           
             a.txFilter = comm.RaisedCosineTransmitFilter( ...
                 'OutputSamplesPerSymbol',a.ss,...
-                'FilterSpanInSymbols',2);
+                'FilterSpanInSymbols',1);
             
             a.rxFilter = comm.RaisedCosineReceiveFilter(...
                 'InputSamplesPerSymbol',a.ss,'DecimationFactor',a.ss,...
-                'FilterSpanInSymbols',2);
+                'FilterSpanInSymbols',1);
            
             a.tx = sdrtx('Pluto', 'CenterFrequency', a.fc,...
-                'BasebandSampleRate', a.rs, 'Gain', -5);
+                'BasebandSampleRate', a.rs, 'Gain', -13);
 
             a.rx = sdrrx('Pluto', 'CenterFrequency', a.fc,...
                 'BasebandSampleRate', a.rs, 'GainSource', ...
                 'Manual', 'Gain', 40, 'SamplesPerFrame',...
                 a.fs, 'OutputDataType', 'single');
             % Increase frequency offset and phase offset to degrade signal
-            a.pfo = comm.PhaseFrequencyOffset('PhaseOffset', 0.5,...
-                'FrequencyOffset', 1000, 'SampleRate',a.rs);
+            a.pfo = comm.PhaseFrequencyOffset('PhaseOffset', 237,...
+                'FrequencyOffset', 223, 'SampleRate',a.rs);
             % Decrease EbNo to degrade signal
-            a.channel=comm.AWGNChannel('SamplesPerSymbol',a.ss,'EbNo',100);
+            a.channel=comm.AWGNChannel('SamplesPerSymbol',a.ss,'EbNo',50);
             
             a.syntonize = comm.CoarseFrequencyCompensator(...
-                'SampleRate',a.rb, 'FrequencyResolution',1);
+                'SampleRate',a.rs, 'FrequencyResolution',1);
             a.synchronize = comm.CarrierSynchronizer( ...
-                'SamplesPerSymbol',1);
+                'SamplesPerSymbol',a.ss);
             a.preamble_detector = comm.PreambleDetector(a.preamble,...
                 'Threshold',3,'Detections','All');
       end
@@ -79,10 +86,9 @@ classdef DSP_class
           
           % applies QAM mapping
           QAM_signal = qammod(signal_dec, a.QAM);
-
+          
           % inerpolation and optimal filter for ISI
-          signal_in = [zeros(a.guard,1); a.preamble; QAM_signal;...
-              a.preamble];          
+          signal_in = [a.preamble; QAM_signal];          
       end
       
       function signal_out = propagate(a, signal_in)
@@ -99,44 +105,49 @@ classdef DSP_class
       function signal_out = simulate(a, signal_in)
           
           % transmitted signal
-          signal_tx = a.txFilter([signal_in; zeros(a.guard,1)]);
+          signal_tx = a.txFilter([signal_in; zeros(a.delay,1)]);
 %           scatterplot(signal_tx)
           
           % received signal
           signal_out = a.rxFilter(a.channel(a.pfo(signal_tx)));
 %           scatterplot(signal_out)
 
-          % signal_out = awgn(signal_in,18,'measured');
+%           signal_out = awgn(signal_in,18,'measured');
+            
+%           signal_out =signal_in;
       end
       
-      function [signal, message_out] = decode(a, signal_out)
+      function [preamble_cond, signal_cond] = conditioning(a, signal_out)
           
           % synchronization of the signal
-          signal_synt = a.syntonize(signal_out);
-          signal_sync = a.synchronize(signal_synt);
-%           figure(1)
-%           scatterplot(signal_sync)
-%           figure(2)
-          plot(10*log10(abs(signal_sync)))
+          signal_sync = a.syntonize(signal_out);
+          signal_sync = a.synchronize(signal_sync);
+          
           % locating the preamble
           [~, correlation] = a.preamble_detector(signal_sync);
+          [~, index] = maxk(correlation, 2);
+          data_end = index(1) + a.mlen;
+          if data_end < length(signal_sync)
+              data_start = index(1) + 1;
+          else
+              data_end = index(2) + a.mlen;
+              data_start = index(2) + 1;
+          end
+          preamble_start = data_start - a.plen;
           
-          [~, indices] = maxk(correlation, 2);
-          data_end = max(indices) - length(a.preamble);
-          data_start = min(indices) + 1;
-          num_char = int32((data_end - data_start + 1)/a.ls);
-          data_end = num_char*a.ls + data_start - 1;
-          preamble_start = data_start - length(a.preamble);
-          
-%           phase correction 2.0
-          preamble_out = signal_sync(preamble_start:data_start-1);
-          phase_offset = round(mean(exp(1i*(angle(a.preamble) - ...
-              angle(preamble_out)))));
-          signal =signal_sync(data_start:data_end).*phase_offset;
-%           signal =signal_sync(data_start:data_end);
-%           scatterplot(signal_out(data_start:data_end
+%         phase correction 2.0 (corrects only by n*pi/4 where n is int)
+          preamble_sync = signal_sync(preamble_start:data_start-1);
 
-          plot(10*log10(abs(signal_sync)))     
+          phase_offset = (mean(exp(1i*(angle(a.preamble(...
+              end-a.plen+1:end)) - angle(preamble_sync)))));
+          signal_cond =signal_sync(data_start:data_end).*phase_offset;
+          preamble_cond = preamble_sync.*phase_offset;
+
+%           signal_cond =signal_sync(data_start:data_end);
+%           preamble_cond = preamble_sync;
+      end 
+      
+      function message_out = decode(a, signal)
           
           z = dec2base(qamdemod(signal,a.QAM), a.QAM);
           z = reshape(z, a.ls, [])';
--- main.m
+++ main.m
@@ -1,27 +1,37 @@
 close ('all'); %clearvars;
 %% Initialization parameters
 
-a = DSP_class();
-a.fc = 900e6;       % carrier frequency in Hz
-a.rb = 30e3;        % symbols (bits) per second
-a.ss = 10;          % samples per symbol
-a.fs = 6e3;         % samples per frame
-a.QAM = 2;          % size of QAM constellation
-a.guard = 400;      % guard length
-a.delay = 500;      % approximate delay of the transmitter
-a = a.setup();
+% message to be transmitted
+message = 'I am happy cuz this is finally working properly but I am still annoyed at whoever is making the pc run slowly';
 
-%% Execution
-message = 'For whoever is slowing down the pc I wish they suffer';     % message to be transmitted
+a = DSP_class();
+a.fc = 868e6;       % carrier frequency in Hz
+a.rb = 50e3;        % symbols (bits) per second
+a.ss = 4;           % samples per symbol
+a.QAM = 4;          % size of QAM constellation
+a.plen = 26;        % preamble length
+a.delay = 100;      % approximate delay of the transmitter (simulation)
+a = a.setup(message);
+
+%% Propagation
 
 signal_in = a.encode(message); % encodes the message
-scatterplot(signal_in)
+
 signal_out = a.propagate(signal_in); % transmits and receives the signal
 
-[signal, message_out] = a.decode(signal_out); % receives and decodes
+%% Processing
 
-plot(10*log10(abs(signal)))
+% syncronizes the signal received and finds preamble
+[preamble_cond, signal_cond] = a.conditioning(signal_out); 
 
-sprintf(message_out)
+message_out = a.decode(signal_cond); % decodes
+
 a.release() % releases system objects
-scatterplot(signal)
+
+% make plots
+sprintf(message_out)
+figure(1)
+plot(real(preamble_cond))
+figure(2)
+plot(real(signal_out))
+scatterplot(signal_cond)
